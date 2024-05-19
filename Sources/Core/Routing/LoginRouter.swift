0c5224dc9b58fd5f638143628651721cf070d26f
ToyaSuzuki <dengyelingmu@gmail.com>
2024-05-19 13:24:51 +0900

    :sparkles: 【iOS】起動時のログイン状態の確認実装

--- Sources/Core/Routing/LoginRouter.swift
+++ Sources/Core/Routing/LoginRouter.swift
@@ -10,5 +10,7 @@ import Foundation
 public class LoginRouter: ObservableObject {
     @Published public var isLogin: Bool = false
     
-    public init() {}
+    public init() {
+        self.isLogin = UserDefaults(suiteName: "group.inemuri")?.string(forKey: "SessionIdKey") != nil
+    }
 }
